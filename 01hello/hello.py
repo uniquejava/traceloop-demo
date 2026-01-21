ce7304166b660cc38b956ca4a39f01a5f32de31c
uniquejava <uniquejava@gmail.com>
2026-01-21 17:49:53 +0800

    use ollama

--- 01hello/hello.py
+++ 01hello/hello.py
@@ -1,11 +1,16 @@
 from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage
 from dotenv import load_dotenv
 import os
+from traceloop.sdk import Traceloop
 
 load_dotenv()
 
+# Traceloop.init(app_name="demo-traceloop-app", disable_batch=True)
+
+# OPENAI_MODEL=ollama:qwen2.5:3b
 model = init_chat_model(model=os.environ["OPENAI_MODEL"])
 
-res = model.invoke("hi")
+res = model.invoke(input=[HumanMessage("Tell me a joke about OpenTelemetry")])
 
-print(res)
+print(res.content)
